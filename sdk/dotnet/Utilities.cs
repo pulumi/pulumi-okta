b79379a9cfebc6fee84bf01cb0bf139074bcca47
stack72 <public@paulstack.co.uk>
2019-12-02 17:13:35 +0200

    Upgrade to pulumi-terraform-bridge v1.4.3

--- sdk/dotnet/Utilities.cs
+++ sdk/dotnet/Utilities.cs
@@ -66,7 +66,7 @@ namespace Pulumi.Okta
         static Utilities()
         {
             var assembly = typeof(Utilities).GetTypeInfo().Assembly;
-            using var stream = assembly.GetManifestResourceStream("Pulumi.Azure.version.txt");
+            using var stream = assembly.GetManifestResourceStream("Pulumi.Okta.version.txt");
             using var reader = new StreamReader(stream ?? throw new NotSupportedException("Missing embedded version.txt file"));
             version = reader.ReadToEnd().Trim();
         }
