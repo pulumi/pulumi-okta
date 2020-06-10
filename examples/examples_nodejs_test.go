096a3421bc63dc2d1a6a1504837b4b480e6b0267
Lee Briggs <lee@leebriggs.co.uk>
2020-06-10 16:12:20 -0700

    prepare for release with GitHub actions

---
+++ examples/examples_nodejs_test.go
@@ -0,0 +1,32 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+// +build nodejs all
+
+package examples
+
+import (
+	"path"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)
+
+func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
+	base := getBaseOptions()
+	baseJS := base.With(integration.ProgramTestOptions{
+		Dependencies: []string{
+			"@pulumi/okta",
+		},
+	})
+
+	return baseJS
+}
+
+func TestAccUserTs(t *testing.T) {
+	skipIfNoPulumiAccessToken(t)
+	test := getJSBaseOptions().
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "user"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
