4e415c4a999669bdbe78081337d8be345092b5fd
aq17 <aqiu@pulumi.com>
2023-02-14 16:39:55 -0800

    add token mapping

--- provider/resources.go
+++ provider/resources.go
@@ -25,6 +25,7 @@ import (
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 )
 
 // all of the token components used below.
@@ -556,6 +557,9 @@ func Provider() tfbridge.ProviderInfo {
 				Source: "auth_server_policy_rule.html.markdown"},
 		})
 
+	err := prov.ComputeDefaults(tfbridge.TokensSingleModule(
+		"okta_", mainMod, tfbridge.MakeStandardToken(mainPkg)))
+	contract.AssertNoError(err)
 	prov.SetAutonaming(255, "-")
 
 	return prov
